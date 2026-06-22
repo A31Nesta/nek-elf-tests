c61b4e48d543990f8034e3c17d214e9036a06d96
A31Nesta <a31nesta@black.observer>
2026-06-22 19:41:20 +0200

    feat: make a function pointer / call testing program

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,16 @@
 
 use core::ffi::{c_char, c_int};
 
+fn fn_ptr_test() -> i32 {
+    42
+}
+
+static FP: fn() -> i32 = fn_ptr_test;
+
 #[unsafe(no_mangle)]
 #[unsafe(link_section = ".text")]
 fn main(_argc: c_int, _argv: *const *const c_char) -> c_int {
-    67
+    FP()
 }
 
 #[panic_handler]
