dc7eafff8a5a778e7772ffc0af4b1bddd65bc286
A31Nesta <a31nesta@black.observer>
2026-07-12 23:03:15 +0200

    feat: remove linker script and make program complex again
    Now the program fucking loads on the ee ess pee thirty two hyphen ess
    three using Expel

--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,19 @@
 use core::ffi::{c_char, c_int};
 
 unsafe extern "C" {
-    // fn nek_serial_print(ptr: *const u8, len: usize);
+    fn nek_serial_print(ptr: *const u8, len: usize);
     fn nek_add(a: i32, b: i32) -> i32;
 }
 
 #[unsafe(no_mangle)]
 #[unsafe(link_section = ".text")]
 fn main(_argc: c_int, _argv: *const *const c_char) -> c_int {
-    // let rust_string = "Hello, World! - From ELF - Rust\n";
-    // let c_string = c"Hello, World! - From ELF - C\n";
+    let rust_string = "Hello, World! - From ELF - Rust\n";
+    let c_string = c"Hello, World! - From ELF - C\n";
 
     unsafe {
-        // nek_serial_print(rust_string.as_ptr(), rust_string.len());
-        // nek_serial_print(c_string.as_ptr(), c_string.count_bytes());
+        nek_serial_print(rust_string.as_ptr(), rust_string.len());
+        nek_serial_print(c_string.as_ptr(), c_string.count_bytes());
 
         let stupid_sum = nek_add(10, 9);
         nek_add(stupid_sum, 2) // to correct the addition :)
