22d333506f2b889be70b0f7ddaa2f9465676b774
A31Nesta <a31nesta@black.observer>
2026-07-17 22:43:21 +0200

    feat: add something that looks like a runtime

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,15 @@
 #![no_std]
 #![no_main]
 
-use core::ffi::{c_char, c_int};
-
 unsafe extern "C" {
     fn nek_serial_print(ptr: *const u8, len: usize);
     fn nek_add(a: i32, b: i32) -> i32;
+
+    fn nek_program_kill(pid: u32);
 }
 
 #[unsafe(no_mangle)]
-#[unsafe(link_section = ".text")]
-fn main(_argc: c_int, _argv: *const *const c_char) -> c_int {
+pub extern "C" fn main() {
     let rust_string = "Hello, World! - From ELF - Rust\n";
     let c_string = c"Hello, World! - From ELF - C\n";
 
@@ -19,9 +18,25 @@ fn main(_argc: c_int, _argv: *const *const c_char) -> c_int {
         nek_serial_print(c_string.as_ptr(), c_string.count_bytes());
 
         let stupid_sum = nek_add(10, 9);
-        nek_add(stupid_sum, 2) // to correct the addition :)
+        nek_add(stupid_sum, 2); // to correct the addition :)
         // 21
     }
+}
+
+#[unsafe(no_mangle)]
+#[unsafe(link_section = ".text")]
+pub extern "C" fn _start() -> ! {
+    let str = "Starting Program\n";
+    unsafe {
+        nek_serial_print(str.as_ptr(), str.len());
+    }
+
+    main();
+    unsafe {
+        nek_program_kill(0);
+    }
+
+    loop {}
 }
 
 #[panic_handler]
