cbce9eca742e9a9183146645e437f15f1bf433b4
A31Nesta <a31nesta@black.observer>
2026-07-25 15:32:43 +0200

    feat: start using the nek-sys crate to simplify syscalls

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,15 @@
 #![no_std]
 #![no_main]
 
-unsafe extern "C" {
-    fn nek_serial_print(ptr: *const u8, len: usize);
-
-    fn sleep(seconds: u32);
-    fn exit();
-}
+use nek_sys::{exit, print, sleep};
 
 #[unsafe(no_mangle)]
 pub extern "C" fn main() {
-    let program_name = "[9-0] ";
-    let newline = "\n";
-
     for i in 0..10 {
-        unsafe {
-            nek_serial_print(program_name.as_ptr(), program_name.len());
-            nek_serial_print(&('9' as u8 - i), 1);
-            nek_serial_print(newline.as_ptr(), newline.len());
-            sleep(2);
-        }
+        print("[0-9 NEK-SYS] ");
+        print(str::from_utf8(&['0' as u8 + i]).unwrap());
+        print("\n");
+        sleep(1);
     }
 }
 
@@ -27,15 +17,10 @@ pub extern "C" fn main() {
 #[unsafe(link_section = ".text")]
 pub extern "C" fn _start() -> ! {
     main();
-    unsafe {
-        exit();
-    }
+    exit();
 
-    let exit_confirmation = "-- should exit --";
     loop {
-        unsafe {
-            nek_serial_print(exit_confirmation.as_ptr(), exit_confirmation.len());
-        }
+        print("-- should exit --");
     }
 }
 
